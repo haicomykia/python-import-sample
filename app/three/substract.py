50f7ef130f4398fa4b6810057fb167e89bc38f46
undefinedSynthesis <undefinedsynthesis@gmail.com>
2022-12-19 21:58:20 +0900

    first commit.

---
+++ app/three/substract.py
@@ -0,0 +1,4 @@
+from app.one import say
+
+def substract(a, b, c):
+    return say.plus(a, b) - c
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
 from app.one import say
+from app.three import substract
 from app import sub
 
 if __name__ == "__main__":
     sub.message()
     say.hello()
     print(f'1 + 3 = {say.plus(1, 3)}')
+
+    print(f'1 + 2 - 3 = {substract.substract(1, 2, 3)}')
 
     print()
 
